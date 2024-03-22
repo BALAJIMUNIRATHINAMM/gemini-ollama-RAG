b5d45c98322a85d95b7d747a673027043de37e30
Balaji Munirathinam <104365597+BALAJIMUNIRATHINAMM@users.noreply.github.com>
2024-03-22 17:16:35 +0530

    Update RAG.py

--- RAG.py
+++ RAG.py
@@ -1,140 +1,104 @@
-import time
-import os
-import joblib
-import streamlit as st
+from langchain.vectorstores import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
 import google.generativeai as genai
+from langchain.document_loaders import TextLoader, DirectoryLoader, PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import Ollama
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.callbacks.manager import CallbackManager
+from langchain.chains import RetrievalQA
 from dotenv import load_dotenv
+import streamlit as st
+import os
+
+if "history" not in st.session_state:
+    st.session_state.history = []
+
 load_dotenv()
-GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
-genai.configure(api_key=GOOGLE_API_KEY)
 
-new_chat_id = f'{time.time()}'
-MODEL_ROLE = 'ai'
-AI_AVATAR_ICON = '✨'
+model_type= 'ollama'
 
-#Header
-st.header("Business Intention Chat-Bot",divider=True)
+# Initializing Gemini
+if(model_type == "ollama"):
+    model = Ollama(
+                    model=<MODEL_NAME>,  # Provide your ollama model name here
+                    callback_manager=CallbackManager([StreamingStdOutCallbackHandler])
+                )
+    
+elif(model_type == "gemini"):
+    model = ChatGoogleGenerativeAI(
+                                model="gemini-pro", 
+                                temperature=0.1, 
+                                convert_system_message_to_human=True
+                            )
+    
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
-# Create a data/ folder if it doesn't already exist
-try:
-    os.mkdir('data/')
-except:
-    # data/ folder already exists
-    pass
+# Vector Database
+persist_directory = <PERSIST_DIR> # Persist directory path
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 
-# Load past chats (if available)
-try:
-    past_chats: dict = joblib.load('data/past_chats_list')
-except:
-    past_chats = {}
+if not os.path.exists(persist_directory):
+    with st.spinner('🚀 Starting your bot.  This might take a while'):
+        # Data Pre-processing
+        pdf_loader = DirectoryLoader("./docs/", glob="./*.pdf", loader_cls=PyPDFLoader)
+        text_loader = DirectoryLoader("./docs/", glob="./*.txt", loader_cls=TextLoader)
+        
+        pdf_documents = pdf_loader.load()
+        text_documents = text_loader.load()
+        
+        splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=0)
+        
+        pdf_context = "\n\n".join(str(p.page_content) for p in pdf_documents)
+        text_context = "\n\n".join(str(p.page_content) for p in text_documents)
 
-# Sidebar allows a list of past chats
-with st.sidebar:
-    st.write('# Past Chats')
-    if st.session_state.get('chat_id') is None:
-        st.session_state.chat_id = st.selectbox(
-            label='Pick a past chat',
-            options=[new_chat_id] + list(past_chats.keys()),
-            format_func=lambda x: past_chats.get(x, 'New Chat'),
-            placeholder='_',
-        )
-    else:
-        # This will happen the first time AI response comes in
-        st.session_state.chat_id = st.selectbox(
-            label='Pick a past chat',
-            options=[new_chat_id, st.session_state.chat_id] + list(past_chats.keys()),
-            index=1,
-            format_func=lambda x: past_chats.get(x, 'New Chat' if x != st.session_state.chat_id else st.session_state.chat_title),
-            placeholder='_',
-        )
-    # Save new chats after a message has been sent to AI
-    # TODO: Give user a chance to name chat
-    st.session_state.chat_title = f'ChatSession-{st.session_state.chat_id}'
+        pdfs = splitter.split_text(pdf_context)
+        texts = splitter.split_text(text_context)
 
-st.info("## Enter your question Below Here!👇 ")
+        data = pdfs + texts
 
-# Chat history (allows to ask multiple questions)
-try:
-    st.session_state.messages = joblib.load(
-        f'data/{st.session_state.chat_id}-st_messages'
-    )
-    st.session_state.gemini_history = joblib.load(
-        f'data/{st.session_state.chat_id}-gemini_messages'
-    )
-    print('old cache')
-except:
-    st.session_state.messages = []
-    st.session_state.gemini_history = []
-    print('new_cache made')
-st.session_state.model = genai.GenerativeModel('gemini-pro')
-st.session_state.chat = st.session_state.model.start_chat(
-    history=st.session_state.gemini_history,
+        print("Data Processing Complete")
+
+        vectordb = Chroma.from_texts(data, embeddings, persist_directory=persist_directory)
+        vectordb.persist()
+
+        print("Vector DB Creating Complete\n")
+
+elif os.path.exists(persist_directory):
+    vectordb = Chroma(persist_directory=persist_directory, 
+                  embedding_function=embeddings)
+    
+    print("Vector DB Loaded\n")
+
+# Quering Model
+query_chain = RetrievalQA.from_chain_type(
+    llm=model,
+    retriever=vectordb.as_retriever()
 )
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(
-        name=message['role'],
-        avatar=message.get('avatar'),
-    ):
-        st.markdown(message['content'])
+for msg in st.session_state.history:
+    with st.chat_message(msg['role']):
+        st.markdown(msg['content'])
 
-# React to user input
-if prompt := st.chat_input('Your message here...'):
-    # Save this as a chat for later
-    if st.session_state.chat_id not in past_chats.keys():
-        past_chats[st.session_state.chat_id] = st.session_state.chat_title
-        joblib.dump(past_chats, 'data/past_chats_list')
-    # Display user message in chat message container
-    with st.chat_message('user'):
+
+prompt = st.chat_input("Say something")
+if prompt:
+    st.session_state.history.append({
+        'role':'user',
+        'content':prompt
+    })
+
+    with st.chat_message("user"):
         st.markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append(
-        dict(
-            role='user',
-            content=prompt,
-        )
-    )
-    ## Send message to AI
-    response = st.session_state.chat.send_message(
-        prompt,
-        stream=True,
-    )
-    # Display assistant response in chat message container
-    with st.chat_message(
-        name=MODEL_ROLE,
-        avatar=AI_AVATAR_ICON,
-    ):
-        message_placeholder = st.empty()
-        full_response = ''
-        assistant_response = response
-        # Streams in a chunk at a time
-        for chunk in response:
-            # Simulate stream of chunk
-            # TODO: Chunk missing `text` if API stops mid-stream ("safety"?)
-            for ch in chunk.text.split(' '):
-                full_response += ch + ' '
-                time.sleep(0.05)
-                # Rewrites with a cursor at end
-                message_placeholder.write(full_response + '▌')
-        # Write full message with placeholder
-        message_placeholder.write(full_response)
 
-    # Add assistant response to chat history
-    st.session_state.messages.append(
-        dict(
-            role=MODEL_ROLE,
-            content=st.session_state.chat.history[-1].parts[0].text,
-            avatar=AI_AVATAR_ICON,
-        )
-    )
-    st.session_state.gemini_history = st.session_state.chat.history
-    # Save to file
-    joblib.dump(
-        st.session_state.messages,
-        f'data/{st.session_state.chat_id}-st_messages',
-    )
-    joblib.dump(
-        st.session_state.gemini_history,
-        f'data/{st.session_state.chat_id}-gemini_messages',
-    )
+    with st.spinner('💡Thinking'):
+        response = query_chain({"query": prompt})
+
+        st.session_state.history.append({
+            'role' : 'Assistant',
+            'content' : response['result']
+        })
+
+        with st.chat_message("Assistant"):
+            st.markdown(response['result'])
